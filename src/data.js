038d3ee5b153c9181191789134d4ab7c8882d662
Malla <malla.nyrhinen@gmail.com>
2024-03-03 12:59:58 +0200

    Add data cities data

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        city: "Tampere",
+        lat: 61.4991,
+        lon: 23.7871
+    },
+    {
+        id: 2,
+        city: "Jyväskylä",
+        lat: 62.2415,
+        lon: 25.7209
+    },
+    {
+        id: 3,
+        city: "Kuopio",
+        lat: 62.8924,
+        lon: 27.677
+    },
+    {
+        id: 4,
+        city: "Espoo",
+        lat: 60.25,
+        lon: 24.6667
+    }
+]
