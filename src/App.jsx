e7397e2d5ea3bb14c7321e76b215b2e19e25e48a
Malla <malla.nyrhinen@gmail.com>
2024-03-03 18:24:04 +0200

    Move api functionality to Weather component

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,19 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
 import Dropdown from "./components/Dropdown";
 import Weather from "./components/Weather";
 import cityData from "./data"
 
 function App() {
-  const apiKey = import.meta.env.VITE_API_KEY;
   const cities = cityData;
-  const URL = "https://api.openweathermap.org/data/2.5/";
 
   const [selectedCity, setSelectedCity] = useState(null);
-  const [currentWeatherData, setCurrentWeatherData] = useState([]);
-  const [forecastedWeatherData, setForecastedWeatherData] = useState([]);
 
-  async function getCurrentWeather(latitude, longitude) {
-    try { 
-      const response = await axios.get(`${URL}weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`);
-      setCurrentWeatherData(prevData => [...prevData, response.data]);
-    } catch(error) {
-      console.log(error.data)
-    }
-  }
-
-  async function getForecastWeather(latitude, longitude) {
-    try {
-      const response = await axios.get(`${URL}forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`);
-      setForecastedWeatherData(prevData => [...prevData, response.data]);
-    } catch (error) {
-      console.log(error.data)
-    }
-  }
-
-  useEffect(() => {
-    for (const city of cities) {
-      getCurrentWeather(city.lat, city.lon);
-      getForecastWeather(city.lat, city.lon);
-    }
-  }, [])
 
   return (
     <>
       <header>Säätutka</header>
       <Dropdown />
-      <Weather />
+      <Weather cities={cities}/>
     </>
   );
 }
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,9 +1,42 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import Current from "./Current";
 import Forecast from "./Forecast";
 
-const Weather = () => {
+const Weather = ({ cities }) => {
+    const apiKey = import.meta.env.VITE_API_KEY;
+    const URL = "https://api.openweathermap.org/data/2.5/";
+
+    const [currentWeatherData, setCurrentWeatherData] = useState([]);
+    const [forecastedWeatherData, setForecastedWeatherData] = useState([]);
+
+    async function getCurrentWeather(latitude, longitude) {
+        try {
+            const response = await axios.get(`${URL}weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`);
+            setCurrentWeatherData(prevData => [...prevData, response.data]);
+        } catch (error) {
+            console.log(error.data)
+        }
+    }
+
+    async function getForecastWeather(latitude, longitude) {
+        try {
+            const response = await axios.get(`${URL}forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`);
+            setForecastedWeatherData(prevData => [...prevData, response.data]);
+        } catch (error) {
+            console.log(error.data)
+        }
+    }
+
+    useEffect(() => {
+        for (const city of cities) {
+            getCurrentWeather(city.lat, city.lon);
+            getForecastWeather(city.lat, city.lon);
+        }
+    }, [])
+
     return (
-        <div>
+        <div className="weather-container">
             <Current />
             <Forecast />
         </div>
